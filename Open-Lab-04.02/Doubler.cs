5048431e8d722a05184deb38ace6fd90890f62a3
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-11-09 11:00:56 +0100

    no hura

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -6,7 +6,15 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            char[] vysledok = new char[original.Length * 2];
+            for (int i = 0; i < original.Length; i++)
+            {
+                char nowchar = original[i];
+                vysledok[(i + 1) * 2 - 2] = nowchar;
+                vysledok[(i + 1) * 2 - 1] = nowchar;
+            }
+            string vysledokstr = new string(vysledok);
+            return vysledokstr;
         }
     }
 }
